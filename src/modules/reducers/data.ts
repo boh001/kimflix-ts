08590d36f44726b42301bfd6dac2fa78789730d8
boh001 <boh001g@gmail.com>
2020-06-15 12:20:09 +0900

    2020-06-15

--- src/modules/reducers/data.ts
+++ src/modules/reducers/data.ts
@@ -30,53 +30,61 @@ type Details = {
   vote_count: number;
   popularity: number;
 };
+
+type IdxMain = Record<number, Main>;
+type IdxDetails = Record<number, Details>;
+
 type DataState = {
   main: {
-    nowPlaying: Main;
-    upcoming: Main;
-    popular: Main;
-    latest: Main;
+    nowPlaying: IdxMain;
+    upcoming: IdxMain;
+    popular: IdxMain;
+    latest: IdxMain;
   };
   details: {
-    detail: Details;
-    similar: Details;
-    cast: Details;
+    detail: IdxDetails;
+    similar: IdxDetails;
+    cast: IdxDetails;
   };
 };
 
 type DataAction = ReturnType<typeof onData> | ReturnType<typeof onDetail>;
 
 const mainContents = {
-  popularity: 0,
-  vote_count: 0,
-  video: false,
-  poster_path: "",
-  id: 0,
-  adult: false,
-  backdrop_path: "",
-  original_language: "",
-  original_title: "",
-  genre_ids: [],
-  title: "",
-  vote_average: 0,
-  overview: "",
-  release_date: "",
+  0: {
+    popularity: 0,
+    vote_count: 0,
+    video: false,
+    poster_path: "",
+    id: 0,
+    adult: false,
+    backdrop_path: "",
+    original_language: "",
+    original_title: "",
+    genre_ids: [],
+    title: "",
+    vote_average: 0,
+    overview: "",
+    release_date: "",
+  },
 };
 const detailContents = {
-  adult: false,
-  backdrop_path: "",
-  genre_ids: [],
-  id: 0,
-  original_language: "",
-  original_title: "",
-  overview: "",
-  poster_path: "",
-  release_date: "",
-  title: "",
-  video: false,
-  vote_average: 0,
-  vote_count: 0,
-  popularity: 0,
+  0: {
+    adult: false,
+    backdrop_path: "",
+    genre_ids: [],
+    id: 0,
+    original_language: "",
+    original_title: "",
+    overview: "",
+    poster_path: "",
+    release_date: "",
+    title: "",
+    video: false,
+    vote_average: 0,
+    vote_count: 0,
+    popularity: 0,
+  },
 };
 
 const initialState: DataState = {
@@ -96,13 +104,19 @@ const initialState: DataState = {
 const ONDATA = "ONDATA" as const;
 const ONDETAIL = "ONDETAIL" as const;
 
-export const onData = (payload: any) => ({ type: ONDATA, payload });
-export const onDetail = (payload: any) => ({ type: ONDETAIL, payload });
+export const onData = (payload: IdxMain) => ({ type: ONDATA, payload });
+export const onDetail = (payload: IdxDetails) => ({ type: ONDETAIL, payload });
 
 export default (state: DataState = initialState, action: DataAction) => {
   const { payload } = action;
   switch (action.type) {
     case ONDATA:
-      return;
+      return payload;
+    case ONDETAIL:
+      return payload;
+    default:
+      console.log("Data error");
+
+      return state;
   }
 };
